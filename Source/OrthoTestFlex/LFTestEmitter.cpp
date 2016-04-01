21a6ddaa26fef16af5493437b28eccff0cbb9cb3
Olle Svanström <svanstromolle@gmail.com>
2016-04-01 17:34:00 +0200

    Added the LiquidFun library to the project

---
+++ Source/OrthoTestFlex/LFTestEmitter.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OrthoTestFlex.h"
+#include "LFTestEmitter.h"
+#include "LiquidFun.h"
+
+
+// Sets default values
+ALFTestEmitter::ALFTestEmitter()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ALFTestEmitter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	b2Vec2 gravity(0.0f, -9.82f);
+
+	b2World world(gravity);
+}
+
+// Called every frame
+void ALFTestEmitter::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
