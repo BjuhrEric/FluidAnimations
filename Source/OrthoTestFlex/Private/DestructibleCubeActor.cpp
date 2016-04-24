183677c5aeec397d88f7ffae55372afa856f980c
Eric Bjuhr <Sweed.Raver@gmail.com>
2016-04-24 14:08:28 +0200

    Does no longer tick each cube every frame

--- Source/OrthoTestFlex/Private/DestructibleCubeActor.cpp
+++ Source/OrthoTestFlex/Private/DestructibleCubeActor.cpp
@@ -8,7 +8,7 @@
 ADestructibleCubeActor::ADestructibleCubeActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 }
 
